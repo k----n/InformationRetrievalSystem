7f1069acb5167c384e0c4770d33a3e192bc2c9fc
k----n <k----n@users.noreply.github.com>
2015-11-14 10:45:36 -0700

    Allow duplicates when building indicies

--- dbload.sh
+++ dbload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sort -u pterms.txt | perl break.pl | db_load -T -t btree pt.idx
-sort -u rterms.txt | perl break.pl | db_load -T -t btree rt.idx
-sort -u scores.txt | perl break.pl | db_load -T -t btree sc.idx
+sort -u pterms.txt | perl break.pl | db_load -T -t btree -c duplicates=1 pt.idx
+sort -u rterms.txt | perl break.pl | db_load -T -t btree -c duplicates=1 rt.idx
+sort -u scores.txt | perl break.pl | db_load -T -t btree -c duplicates=1 sc.idx
 db_load -f reviews.txt -T -t hash rw.idx
