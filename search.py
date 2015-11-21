34de1b6f1788929d046f547588d19a491e797c04
PiratePlex <skchang@ualberta.ca>
2015-11-20 22:36:56 -0700

    Added searching for terms. TODO integer searches

--- search.py
+++ search.py
@@ -19,30 +19,45 @@ scoresCursor = scores.cursor()
 
 #search = input("input querey u fuk: ")
 #return (pterms,rterms,pprice,rscore,rdate,part_terms,terms)
-search = "pprice < 60 pprice > 30 clothing"
+search = "pprice < 60 pprice > 30 clothing rscore < 3 r:funchuck p:cow blue%"
 
 parsedSearch = queryData(search)
+termLengthTable = []
 print(parsedSearch)
-searchIndex = 0
-while searchIndex < len(parsedSearch):
-    if len(parsedSearch[searchIndex])>0:
-        for termsIndex in range(0, len(parsedSearch[searchIndex])):
-            if searchIndex == 0:
-                encodedTerm = (parsedSearch[searchIndex][termsIndex]).encode()
-                ptermsID = pterms.get(encodedTerm)
-
-            if searchIndex == 1:
-                encodedTerm = (parsedSearch[searchIndex][termsIndex]).encode()
-                rtermsID = rterms.get(encodedTerm)
-
-            if searchIndex == 2:
-                length = 0
-                termstermsIndex = len(parsedSearch[6][length])
-                encodedTerm = (parsedSearch[6][termstermsIndex]).encode()
-
-    searchIndex+=1
-
-
+# make a list to check the number of terms in the parsedSearch
+termLengthTable.append(len(parsedSearch[0]))    # length of pterms
+termLengthTable.append(len(parsedSearch[1]))    # length of rterms
+termLengthTable.append(len(parsedSearch[5]))    # length of part_terms
+termLengthTable.append(len(parsedSearch[6]))    # length of terms
+print(termLengthTable)
+resultIDs = []  # contains all the (binary) IDs of valid results
+index = 0
+for length in termLengthTable:
+    if index == 0 and length != 0:  # if pterms has something, search in pterms.idx only
+        ptermsLength = termLengthTable[0]
+        for ptermsIndex in range (0, ptermsLength):
+            encodedTerm = (parsedSearch[0][ptermsIndex]).encode()
+            resultIDs.append(pterms.get(encodedTerm))
+    elif index == 1 and length !=0:   # if rterms has something, search in rterms.idx only
+        rtermsLength = len(parsedSearch[1])
+        for rtermsIndex in range(0, rtermsLength):
+            encodedTerm = (parsedSearch[1][rtermsIndex]).encode()
+            resultIDs.append(rterms.get(encodedTerm))
+    elif index == 2 and length != 0:  # if part_terms has something, search for partial matching strings in pterms.idx and rterms.idx
+        part_termsLength = len(parsedSearch[5])
+        for part_termsIndex in range(0, part_termsLength):
+            term = parsedSearch[5][part_termsIndex].strip("%")
+            encodedTerm = term.encode()
+            #encodedTerm = (parsedSearch[5][part_termsIndex]).encode()
+            resultIDs.append(rtermsCursor.get(encodedTerm, db.DB_SET_RANGE))
+    elif index == 3 and length != 0:    # if terms has something, search for the string in both pterms.idx and rterms.idx
+        termsLength = termLengthTable[3]
+        for termsIndex in range(0, termsLength):
+            encodedTerm = parsedSearch[6][termsIndex].encode()
+            resultIDs.append(pterms.get(encodedTerm))
+            resultIDs.append(rterms.get(encodedTerm))
+    index+=1
+print(resultIDs)
 
 
 #iter = reviewsCursor.first()
