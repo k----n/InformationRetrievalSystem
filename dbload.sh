1a3bf93c0096d9735f712685f14fa1b9b640c6d3
k----n <k----n@users.noreply.github.com>
2015-11-23 22:34:03 -0700

    Update dbload.sh

--- dbload.sh
+++ dbload.sh
@@ -3,4 +3,4 @@
 sort -u pterms.txt | perl break.pl | db_load -T -t btree -c duplicates=1 pt.idx
 sort -u rterms.txt | perl break.pl | db_load -T -t btree -c duplicates=1 rt.idx
 sort -u scores.txt | perl break.pl | db_load -T -t btree -c duplicates=1 sc.idx
-cat reviews.txt | perl break.pl | db_load -f -T -t hash rw.idx
+cat reviews.txt | perl break.pl | db_load -T -t hash rw.idx
